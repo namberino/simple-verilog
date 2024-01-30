7ea7bb045aa774d7e5083d3ee1e1bc53f0f3e8e7
Nam <namberinos@gmail.com>
2024-01-30 19:30:38 +0700

    Add debounce to button counter

--- button_counter/counter.v
+++ button_counter/counter.v
@@ -1,13 +1,75 @@
 module counter(
-	input[1:0] pmod,
+	input rst_button,
+	input inc_button,
+	input clk,
+
 	output reg[3:0] led
 );
 
 	wire rst;
-	wire clk;
+	wire inc;
+
+	localparam STATE_HIGH = 2'd0;
+	localparam STATE_LOW = 2'd1;
+	localparam STATE_WAIT = 2'd2;
+	localparam STATE_PRESSED = 2'd3;
+	localparam MAX_CLK_COUNT = 20'd480000;
+
+	reg[1:0] state;
+	reg[20:0] clk_count;
 	
-	assign clk = ~pmod[1];
-	assign rst = ~pmod[0];
+	assign rst = ~rst_button;
+	assign inc = ~inc_button;
+
+	// state transition logic
+	always @ (posedge clk or posedge rst)
+	begin
+		if (rst == 1'b1)
+		begin
+			state = STATE_HIGH;
+		end else
+		begin
+			case (state)
+				STATE_HIGH:
+					begin
+						if (inc == 1'b0)
+						begin
+							state = STATE_LOW;
+						end
+					end
+
+				STATE_LOW:
+					begin
+						if (inc == 1'b1)
+						begin
+							state = STATE_WAIT;
+						end
+					end
+
+				STATE_WAIT:
+					begin
+						if (clk_count == MAX_CLK_COUNT)
+						begin
+							if (inc == 1'b1)
+							begin
+								state = STATE_PRESSED;
+							end else
+							begin
+								state = STATE_HIGH;
+							end
+						end
+					end
+
+				STATE_PRESSED:
+					begin
+						led <= led + 1;
+						state = STATE_HIGH;
+					end
+
+				default: state = STATE_HIGH;
+			endcase
+		end
+	end
 
 	// count up on clock rising edge or reset on reset button push
 	always @ (posedge clk or posedge rst)
@@ -15,9 +77,13 @@ module counter(
 		if (rst == 1'b1)
 		begin
 			led <= 4'b0;
+			clk_count = 20'd0;
 		end else
 		begin
-			led <= led + 1'b1;
+			if (state == STATE_WAIT)
+			begin
+				clk_count <= clk_count + 1;
+			end
 		end
 	end
 endmodule
